81ec45b907f3492f5583dcf611337ca6b678a038
Matt Kadlubowski <xeho91@pm.me>
2021-08-25 23:03:11 +0800

    build(commitlint): 🔧 Initial setup
    (./package.json):
    ➕ Added "commitlint" and "@commitlint/config-conventional" as devDependencies
    ➕ Added new script "lint:commit"
    
    🔧 Added a new configuration file (./.commitlintrc.js).
    
    👷 Added a new job "commitlint" to the workflow "Continuous Integration".
    
    🥚 Added a new Visual Studio Code extension recommendation.
    
    Co-authored-by: lax0248917 <zach_a_king@outlook.com>

---
+++ .commitlintrc.js
@@ -0,0 +1,15 @@
+// https://commitlint.js.org/
+
+/** @type { import("commitlint/types").UserConfig } */
+const commitlintConfig = {
+	// TODO: Configure when we decide on how do we generate changelogs
+	// https://www.npmjs.com/package/conventional-changelog-conventionalcommits
+	// parserPreset: "conventional-changelog-conventionalcommits",
+
+	// https://github.com/conventional-changelog/commitlint/
+	extends: ["@commitlint/config-conventional"],
+
+	helpUrl: "https://github.com/terminal-nerds/terminal-nerd/wiki/%F0%9F%92%8C-Git-commits-convention"
+};
+
+module.exports = commitlintConfig;
